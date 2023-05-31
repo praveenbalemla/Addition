5722d588221cb76f98422cb4511f1d948c02d269
praveen <praveenbalemla999@gmail.com>
2023-05-31 18:53:11 +0530

    addition commit

---
+++ src/com/tcs/add/Addition.java
@@ -0,0 +1,16 @@
+package com.tcs.add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 =10;
+		int num2=20;
+		int result;
+		
+		result=num1+num2;
+		System.out.println("Addition of two numbers ...."+result);
+
+	}
+
+}
